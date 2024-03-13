181859b38326faed02a5e982c2685df4006721a5
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-03-13 22:19:27 +0500

    addd

---
+++ public/javascript/app.js
@@ -0,0 +1,20 @@
+
+
+let vertikalTab  = document.querySelectorAll('#vertikalTab')
+let vertikalTabTitle  = document.querySelectorAll('#vertikalTab h3')
+let vertikalTabText  = document.querySelectorAll('#vertikalTab #vertikalTabText')
+
+
+vertikalTabText.forEach(function(item , index){
+  item.classList.add('h-0')
+  vertikalTab[index].classList.add('opacity-45')
+})
+
+vertikalTabTitle.forEach(function(item , index){
+  item.addEventListener('click',function(){
+    vertikalTab.forEach(function(item , index){
+      item.classList.remove('active')
+    })
+    vertikalTab[index].classList.add('active')
+  })
+})
